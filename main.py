87807811f72d0acb204bcd7b40be72f4c847e151
Augusto <33730078+augustodoidin@users.noreply.github.com>
2018-06-04 11:33:53 -0300

    Update 2.5
    de txt pra sql

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 print("=====================================================");
 print("TXT ComboList To MYSQL");
-print("Ver: 2.0");
+print("Ver: 2.5");
 print("Discord: AugustoDoidin#2326");
 print("Estado: Funcionando");
 print("=====================================================");
 db = input("Digite a Database do MYSQL: ")
 tabela = input("Digite a Tabela que deve ser usada: ")
 coluna = input("Digite a Coluna que deve ser usada: ")
-txt = input("Digite o nome do TXT em que a conversão será salva: ")
+txt = input("Digite o nome do arquivo SQL em que a conversão será salva: ")
 print("=====================================================");
 print("Digite as contas abaixo");
 print("Para sair digite: sair");
 print("=====================================================");
 conta = ""
 contador = 0
-arq = open(txt + ".txt", "w")
+sql = open(sql + ".sql", "w")
 while(conta != "sair"):
     conta = input()
     if(conta != "sair"):
@@ -23,7 +23,7 @@ while(conta != "sair"):
         contador = contador + 1
 arq.close()
 if(contador != 1):
-    print(contador, "contas foram convertidas e salvas no txt: " + txt + ".txt")
+    print(contador, "contas foram convertidas e salvas no arquivo: " + sql + ".sql")
 else:
-    print(contador, "conta foi convertida e salva no txt: " + txt + ".txt")
+    print(contador, "conta foi convertida e salva no arquivo: " + sql + ".sql")
     
