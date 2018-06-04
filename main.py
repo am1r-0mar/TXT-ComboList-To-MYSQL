8a6e79a08a241206d355f092bc586a2293b33d37
Augusto <33730078+augustodoidin@users.noreply.github.com>
2018-06-04 12:23:19 -0300

    Text Updated.
    Text Updated.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 print("=====================================================");
 print("TXT ComboList To MYSQL");
 print("Ver: 2.5");
@@ -9,7 +10,7 @@ tabela = input("Digite a Tabela que deve ser usada: ")
 coluna = input("Digite a Coluna que deve ser usada: ")
 sql = input("Digite o nome do arquivo SQL em que a conversão será salva: ")
 print("=====================================================");
-print("Digite as contas abaixo");
+print("Digite as contas abaixo ou cole todo seu txt com quebra de linha.");
 print("Para sair digite: sair");
 print("=====================================================");
 conta = ""
@@ -26,4 +27,5 @@ if(contador != 1):
     print(contador, "contas foram convertidas e salvas no arquivo: " + sql + ".sql")
 else:
     print(contador, "conta foi convertida e salva no arquivo: " + sql + ".sql")
+time.sleep(5)
     
